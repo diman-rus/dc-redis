20b390c4cfb5ea1e299dfad1fbde6faac970ff99
diman-rus <diman.rus@mail.ru>
2021-01-31 22:17:37 +0700

    Modify Docker-Compose Redis Cluster

--- start.sh
+++ start.sh
@@ -1,20 +1,27 @@
-!#/bin/bash
+#!/bin/bash
 
 #Install redis-cli on local machine
-#yum install redis-cli -y
+yum install redis-cli -y
 
 #Change dir to .
 cd /
 
 #Make dir
-#mkdir Docker-C && 
-cd  /Docker-C
+mkdir Docker-C 
+cd /Docker-C
 
 #Clone from repository
-#git clone https://github.com/diman-rus/dc-redis.git
+git clone https://github.com/diman-rus/dc-redis.git
 
-cd /dc-redis
-#docker-compose build
+#Go to work directory
+cd /Docker-C/dc-redis
+
+#Build container
+docker-compose build
+
+#Start cluster
 docker-compose up -d
 
-redis-cli -h 173.17.0.1 -p 7000 -c "CLUSTER INFO"
+#Enter cluster info
+echo [Enter in next string - CLUSTER INFO]
+redis-cli -h 173.17.0.1 -p 7000
